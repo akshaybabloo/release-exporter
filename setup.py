62a7846d57d37d9573ad9cb1c346c1bfe30a4810
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 12:54:43 +1300

    files init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='release-exporter',
+    version='0.0.1',
+    packages=['tests', 'release-exporter'],
+    url='https://github.com/akshaybabloo/release-exporter',
+    license='MIT',
+    author='Akshay Raj Gollahalli',
+    author_email='akshay@gollahalli.com',
+    description='Release exporter for GitHub, GitLab and Bitbucket.'
+)

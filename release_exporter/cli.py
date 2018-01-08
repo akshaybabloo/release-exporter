7bbd4f4be438f5d15ba9688da0cb4541415e52d9
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 20:37:43 +1300

    flags can now interact with other flags

--- release_exporter/cli.py
+++ release_exporter/cli.py
@@ -1,4 +1,7 @@
 import click
+import os
+
+from release_exporter.github import github_format
 
 
 @click.group()
@@ -8,8 +11,15 @@ import click
 @click.option('--tags', help='Range of tags.', default='all')
 @click.option('--url',
               help="URL of your repository. This is optional if your current directory has .git folder with remote url.")
-def cli(provider, repo, token, tags, url):
-    pass
+@click.option('--location', help='Where do you want to save your file.', default=os.getcwd())
+@click.pass_context
+def cli(ctx, provider, repo, token, tags, url, location):
+    ctx.obj['provider'] = provider
+    ctx.obj['repo'] = repo
+    ctx.obj['token'] = token
+    ctx.obj['tags'] = tags
+    ctx.obj['url'] = url
+    ctx.obj['location'] = location
 
 
 @cli.command(help='Creates .rex file.')
@@ -18,17 +28,18 @@ def init():
 
 
 @cli.command(help='Creates markdown file.')
-@click.option('--location', help='Where do you want to save your file.')
-def markdown(location):
+@click.pass_context
+def markdown(ctx):
+    github_format(force=True, token="c7933996ed553368b1928d1d1c4d78a5c850675f", location=ctx.obj['location'], url='github.com:akshaybabloo/gollahalli-com').process()
 
 
 @cli.command(help='Creates JSON file.')
-@click.option('--location', help='Where do you want to save your file.')
-def json(location):
+@click.pass_context
+def json(ctx):
     pass
 
 
 @cli.command(help='Creates YAML file.')
-@click.option('--location', help='Where do you want to save your file.')
-def yaml(location):
+@click.pass_context
+def yaml(ctx):
     pass

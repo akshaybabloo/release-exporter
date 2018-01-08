7d24f0254d2391285a08b6fb15ed92be2882ce35
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 19:20:05 +1300

    --url added

--- release_exporter/cli.py
+++ release_exporter/cli.py
@@ -6,7 +6,9 @@ import click
 @click.option('--repo', help='Your repository name.')
 @click.option('--token', help='Token number if its a private repository.', default=None)
 @click.option('--tags', help='Range of tags.', default='all')
-def cli(provider, repo, token, tags):
+@click.option('--url',
+              help="URL of your repository. This is optional if your current directory has .git folder with remote url.")
+def cli(provider, repo, token, tags, url):
     pass
 
 

de94b86b7e8db2e582ec084fad6ff8042cf1e8d8
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 20:49:38 +1300

    header, body and footer template added

--- release_exporter/base.py
+++ release_exporter/base.py
@@ -9,6 +9,28 @@ FILE_TYPE = {
     'yaml': '.yaml'
 }
 
+HEADER = """
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+"""
+
+body = """
+
+## [{tag}] - {date}
+
+{content}
+
+"""
+
+FOOTER = """
+[{tag}]: {url}
+"""
+
 
 class RequestBase:
     """

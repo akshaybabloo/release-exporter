476abadef5957c1e9230b27efa7b1a56a7dc8304
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 19:59:34 +1300

    get_repo_url_info updated

--- release_exporter/utils.py
+++ release_exporter/utils.py
@@ -1,3 +1,8 @@
+import os
+import configparser
+from giturlparse import parse
+from .exceptions import ParserError
+
 Note = """
     ----------------------
             Details
@@ -13,5 +18,13 @@ def check_provider():
     pass
 
 
-def get_repo_url():
-    pass
+def get_repo_url_info(location=os.getcwd(), url=None):
+    if url is None:
+        config = configparser.ConfigParser()
+        config.read(location + os.sep + '.git' + os.sep + 'config')
+        if 'remote "origin"' in config.sections():
+            return parse(config['remote "origin"']['url'])
+        else:
+            raise ParserError('Git config file does not exist please provide the repository url by using --url.')
+    else:
+        return parse(url + '.git')

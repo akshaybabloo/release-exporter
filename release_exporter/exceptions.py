d0c1b641be225cdc69a71e439baec2ed7793d120
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-08 19:59:43 +1300

    ParserError added

--- release_exporter/exceptions.py
+++ release_exporter/exceptions.py
@@ -1,9 +1,20 @@
 class FileExists(Exception):
     """
-    This exception is raised when a file exists..
+    This exception is raised when a file exists.
     """
 
     def __init__(self, message, errors=None):
         super(FileExists, self).__init__(message)
+
+        self.errors = errors
+
+
+class ParserError(Exception):
+    """
+    This exception is raised Git config file does not exist.
+    """
+
+    def __init__(self, message, errors=None):
+        super(ParserError, self).__init__(message)
 
         self.errors = errors

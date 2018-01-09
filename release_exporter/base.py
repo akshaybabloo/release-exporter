a29d52d41456c91d04f0d8520f836db0228dbcfb
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-09 16:53:30 +1300

    creade class methods for header, body and footer

--- release_exporter/base.py
+++ release_exporter/base.py
@@ -9,28 +9,6 @@ FILE_TYPE = {
     'yaml': '.yaml'
 }
 
-HEADER = """
-# Changelog
-
-All notable changes to this project will be documented in this file.
-
-The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
-and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
-
-"""
-
-body = """
-
-## [{tag}] - {date}
-
-{content}
-
-"""
-
-FOOTER = """
-[{tag}]: {url}
-"""
-
 
 class RequestBase:
     """
@@ -58,10 +36,16 @@ class FormatBase(RequestBase):
 
     def __init__(self, location=os.getcwd(), file_name='CHANGELOG', force=False, file_type='markdown', *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.tag = None
+        self.date = None
+        self.content = None
+        self.compare = None
+
         self.location = location
         self.file_name = file_name
         self.force = force
         self.file_type = file_type
+        self.all_content = []
 
         if not force:
             if pathlib.Path(location + os.sep + file_name + self._get_file_type()).is_file():
@@ -84,4 +68,31 @@ class FormatBase(RequestBase):
         pass
 
     def process(self):
+        pass
+
+    @classmethod
+    def _header(cls):
+        return """\
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+"""
+
+    def _body(self):
+        return """\
+
+## [{tag}] - {date}
+
+{content}
+""".format(tag=self.tag, date=self.date, content=self.content)
+
+    def _footer(self):
+        return """\
+[{tag}]: {url}
+""".format(tag=self.tag, url=self.url)
+
+    def _compare(self):
         pass

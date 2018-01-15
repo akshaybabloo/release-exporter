db2e49d7c59b1ac010f3c6a3cbc1af5d25991c2f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-15 17:50:38 +1300

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import codecs
+import os
 from setuptools import setup
+
+from release_exporter import version
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_requirements(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read().splitlines()
+
 
 setup(
     name='release-exporter',
-    version='0.0.1',
-    packages=['tests', 'release-exporter'],
+    version=version(),
+    install_requires=get_requirements('requirements.txt'),
+    packages=['tests', 'release_exporter'],
     url='https://github.com/akshaybabloo/release-exporter',
     license='MIT',
     author='Akshay Raj Gollahalli',
     author_email='akshay@gollahalli.com',
-    description='Release exporter for GitHub, GitLab and Bitbucket.'
+    description='Release exporter for GitHub, GitLab and Bitbucket.',
+    entry_points={
+        'console_scripts': [
+            'rex = release_exporter:main'
+        ]
+    }
 )

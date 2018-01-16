ffa53131e129e694216b5fada68f734a5dd21e3e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-16 17:20:47 +1300

    Long description and changelog added to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,31 @@ import codecs
 import os
 from setuptools import setup
 
-from release_exporter._version import version
+from release_exporter import __version__
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert(here + 'README.md', 'rst')
+    long_description = long_description.replace("\r", "")  # Do not forget this line
+
+    changelog = pypandoc.convert(here + 'CHANGELOG.md', 'rst')
+    changelog = changelog.replace('\r', "")
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+
+    # pandoc is not installed, fallback to using raw contents
+    with io.open(here + 'README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
+    with io.open(here + 'CHANGELOG.md', encoding="utf-8") as f:
+        changelog = f.read()
+
+
 
 
 def get_requirements(*parts):
@@ -13,7 +35,7 @@ def get_requirements(*parts):
 
 setup(
     name='release-exporter',
-    version=version(),
+    version=__version__,
     install_requires=get_requirements('requirements.txt'),
     packages=['tests', 'release_exporter'],
     url='https://github.com/akshaybabloo/release-exporter',
@@ -21,6 +43,7 @@ setup(
     author='Akshay Raj Gollahalli',
     author_email='akshay@gollahalli.com',
     description='Release exporter for GitHub and GitLab.',
+    long_description= long_description + '\n\n' + changelog,
     keywords=['changelog', 'releases'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -37,4 +60,5 @@ setup(
             'rex = release_exporter:main'
         ]
     },
+    cha
 )

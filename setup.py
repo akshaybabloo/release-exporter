e542d3cb9289ed4965b04a77034b6d8774d9050a
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-01-23 16:13:17 +1300

    version import updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import codecs
 import os
 from setuptools import setup
 
-from release_exporter._version import version
+from release_exporter.version import __version__
 
 here = os.path.abspath(os.path.dirname(__file__)) + os.sep
 
@@ -12,7 +12,7 @@ def get_requirements(*parts):
 
 setup(
     name='release-exporter',
-    version=version(),
+    version=__version__,
     install_requires=get_requirements('requirements.txt'),
     packages=['tests', 'release_exporter'],
     url='https://blog.gollahalli.com/export-your-github-and-gitlab-releases-as-a-changelog/',

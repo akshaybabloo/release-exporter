a467580109215ea954ca8563139e1c3c3e02e63c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-03-01 20:48:42 +1300

    Init object partially completed

--- release_exporter/utils.py
+++ release_exporter/utils.py
@@ -1,12 +1,16 @@
 import configparser
 import datetime
 import os
+from pathlib import Path
 
 import dateutil.parser
 from giturlparse import parse
 from tabulate import tabulate
 
+from release_exporter import version
 from release_exporter.exceptions import ParserError
+
+CONFIG_FILE_NAME = '.rex'
 
 
 def get_repo_url_info(location=os.getcwd(), repo_url=None):
@@ -110,3 +114,86 @@ def description(provider=None, repo_name=None, tags_number=None):
     ]
 
     return tabulate(table, tablefmt="grid")
+
+
+class Init:
+    """
+    Creates ``.rex`` and ``CHANGELOG`` file depending on the format given, if no format if given ``CHANGELOG.md`` is
+    created.
+
+    >>> rex init
+    Created .rex config file at <user home path>
+    """
+
+    def __init__(self, location=Path.home()):
+        self.location = location
+
+        self.config_file_path = Path(os.path.join(self.location, CONFIG_FILE_NAME))
+
+    @property
+    def config(self):
+
+        if self.config_file_path.is_file():
+            config_file = configparser.ConfigParser()
+            config_file.read(self.config_file_path)
+            return self.config_file_path, config_file
+        else:
+            self._init_config()
+
+        return self.config_file_path
+
+    def _init_config(self):
+
+        config = configparser.ConfigParser()
+
+        config['DEFAULT'] = {
+            'Version': version.__version__,
+            'CheckCycle': 10,
+            'GitHubKey': '',
+            'GitLabKey': ''
+        }
+
+        with open(self.config_file_path, 'w') as configfile:
+            config.write(configfile)
+
+    @config.setter
+    def config(self, config_dict):
+
+        assert type(config_dict) == dict
+
+        self.config_file_path, config_file = self.config
+
+        exists, config, dict_key = self._check_config_property(config_dict)
+
+        if not exists:
+            if config_dict[dict_key]['Key']:
+                value = config['DEFAULT']['githubkey'] if config_dict[dict_key]['Provider'] is 'github' else \
+                    config['DEFAULT']['gitlabkey']
+                config_dict[dict_key]['Key'] = value
+
+            config.read_dict(config_dict)
+            with open(self.config_file_path, 'w') as configfile:
+                config.write(configfile)
+
+    def _check_config_property(self, config_dict):
+        config = configparser.ConfigParser()
+        config.read(self.config_file_path)
+
+        dict_key = next(iter(config_dict.keys()))
+        exists = dict_key in config.sections()
+
+        return exists, config, dict_key
+
+
+if __name__ == '__main__':
+    a = Init()
+    # print(a.config)
+    # a.config = {
+    #     'github:release-exporter': {
+    #         'Key': True,
+    #         'URL': 'https://',
+    #         'RepoName': 'release-exporter',
+    #         'Provider': 'github'
+    #     }
+    # }
+    print(a.config)
